9a17a843757250b8fcf8f2a30a71487bedc4b12b
liu-cody96 <callofliu@gmail.com>
2023-01-27 00:49:08 -0500

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -6,7 +6,6 @@ DEBUG = True
 
 SERVER_NAME = 'localhost:8000'
 
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
+DB_URL = os.getenv('DB_URL')
 
-SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASS}@kashin.db.elephantsql.com/{DB_USER}'
+SQLALCHEMY_DATABASE_URI = DB_URL
