757811ed552f8a521079a004662fee3953379527
liu-cody96 <callofliu@gmail.com>
2023-02-13 17:50:13 -0500

    fix broken test :)

--- backend/receiptscanner/tests/test_routes.py
+++ backend/receiptscanner/tests/test_routes.py
@@ -2,4 +2,4 @@
 class TestPage(object):
     def test_home_route(self, client):
         resp = client.get('/')
-        assert b'Welcome to Receipt5canner.' == resp.data # b specifies byte string
+        assert b'Welcome to ReceiptScanner.' == resp.data # b specifies byte string
